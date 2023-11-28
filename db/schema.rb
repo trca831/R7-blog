b3ffe0344f3c944b3961f29910eed6e18bb11404
Tracy Cano <tracycano3.14@gmail.com>
2023-11-28 09:20:47 -0800

    Adding changes for creating a model video

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_28_034739) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_28_171808) do
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
