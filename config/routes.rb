a13ef7c3fd842c0b50ab933645031563d788d192
Tracy Cano <tracycano3.14@gmail.com>
2023-12-02 12:08:03 -0800

    Changes for video called An Edit Route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post '/pages', to: 'pages#create'
   get '/pages/new', to: 'pages#new', as: 'new_page'
   get '/pages/:id', to: 'pages#show', as: 'page'
+  get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
