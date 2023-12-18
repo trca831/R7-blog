b73989c75aaa91cc5b69cc9c61f4033143157c70
Tracy Cano <tracycano3.14@gmail.com>
2023-12-18 13:34:43 -0800

    Delete Action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,11 @@
 class PagesController < ApplicationController
+    before_action :set_page, only: [:show, :edit, :update, :destroy]
+
     def index
         @pages = Page.all
     end
 
     def show
-        @page = Page.find(params[:id])
     end
 
     def new 
@@ -18,14 +19,17 @@ class PagesController < ApplicationController
     end
 
     def edit
-        @page = Page.find(params[:id])
     end
 
     def update
-        @page = Page.find(params[:id])
         @page.update(page_params)
         redirect_to @page
     end
+
+    def destroy
+        @page.destroy
+        redirect_to pages_path
+      end
 
     private
 
@@ -33,4 +37,7 @@ class PagesController < ApplicationController
         params.require(:page).permit(:title, :body, :slug)
     end
 
+    def set_page
+    @page = Page.find(params[:id])
+    end
 end
