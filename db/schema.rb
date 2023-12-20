6507fb9fae867c8509556d534bb8dfc426c669dd
Tracy Cano <tracycano3.14@gmail.com>
2023-12-19 22:22:57 -0800

    updating the model vid from week 6

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_19_214732) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_20_061756) do
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "pages", force: :cascade do |t|
@@ -30,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_19_214732) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
   end
 
 end
