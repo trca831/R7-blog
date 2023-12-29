59d94486739e0a7a1fc933787fce1487dd19904a
Tracy Cano <tracycano3.14@gmail.com>
2023-12-29 12:54:53 -0800

    variations of foreign migrations video ... deleted create comments table migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_12_20_061756) do
-  create_table "comments", force: :cascade do |t|
-    t.text "content"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "post_id"
-    t.index ["post_id"], name: "index_comments_on_post_id"
-  end
-
   create_table "pages", force: :cascade do |t|
     t.string "title"
     t.text "body"
